657d8860d3178dca9535ef361cbf623b1b82f71b
Shuung <qmoy2510@gmail.com>
2026-03-20 15:31:29 +0900

    ㅇㅇ

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import AboutPage from './pages/AboutPage'
 import PortfolioPage from './pages/PortfolioPage'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<AboutPage />} />
         <Route path="/portfolio" element={<PortfolioPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
